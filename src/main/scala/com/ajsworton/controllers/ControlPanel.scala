3757ebff67a219a205c73306384812da054ea791
Alexander Worton <kesomir@btinternet.com>
2017-03-03 16:40:35 +0000

    Fixed databinding for centreNumber

--- src/main/scala/com/ajsworton/controllers/ControlPanel.scala
+++ src/main/scala/com/ajsworton/controllers/ControlPanel.scala
@@ -12,18 +12,18 @@ import javafx.scene.input.KeyEvent
 
 import com.ajsworton.Config.Settings
 
-import scalafx.beans.property.StringProperty
-
 /**
   * Created by aworton on 03/03/17.
   */
 class ControlPanel extends Initializable{
 
-  var centreNumber: String = Settings.centreNumber
+  val centreNumber: ObjectProperty[String] = new SimpleObjectProperty(Settings.centreNumber)
 
-  def getCentreNumber: String = centreNumber
+  def getCentreNumber: String = centreNumber.get
 
-  def setCentreNumber(number: String): Unit = centreNumber = number
+  def centreNumberProperty: ObjectProperty[String] = centreNumber
+
+  def setCentreNumber(number: String): Unit = centreNumber.set(number)
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
   }
